8446d609b386ca232ae143fe623bc8a61188531a
RafaelDias108 <rafaelsd70@gmail.com>
2026-05-23 01:10:11 -0300

    feat(controller): integrar PostService e adicionar endpoints de atualização e deleção

--- controllers/post.py
+++ controllers/post.py
@@ -1,29 +1,29 @@
 
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, status
+from services.posts import PostService
 from views.post import PostOut
-from schemas.post import PostIn
-from models.post import posts
-from database import database
+from schemas.post import PostIn, PostUpdateIn
 
 router = APIRouter(prefix="/posts")
 
+post_service = PostService()
+
 @router.get("", response_model=list[PostOut], description="Get all posts")
 async def read_posts(published: bool = True, limit: int = 10, skip: int = 0):
-    query = posts.select()
-    return await database.fetch_all(query=query)
+    return await post_service.get_all(published=published, limit=limit, skip=skip)
 
 @router.get("/{id}", response_model=PostOut, description="Get post by id")
 async def read_post(id: int):
-    query = posts.select().where(posts.c.id == id)
-    post = await database.fetch_one(query=query)
-
-    if not post:
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
-    return post
+    return await post_service.get_by_id(id)
 
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=PostOut, description="Create a new post")
-async def create_posts(post: PostIn):
-    query = posts.insert().values(title=post.title, content=post.content, published=post.published)
-    last_record_id = await database.execute(query)
-    return {**post.model_dump(), "id": last_record_id}
-    
+async def create_post(post: PostIn):
+    return await post_service.create(post)
+
+@router.patch("/{id}", response_model=PostOut, description="Update a post")
+async def update_post(id: int, post: PostUpdateIn):
+    return await post_service.update(id=id, post=post)
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, description="Delete a post", response_model=None)
+async def delete_post(id: int):
+    return await post_service.delete(id=id) 
